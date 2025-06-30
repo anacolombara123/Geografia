cf7b1a5be99c25f7b6312cc75621f217e03d7757
anacolombara123 <ana.colombara.alves@escola.pr.gov.br>
2025-06-30 08:02:47 -0300

    Tudocerto

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Marvel',
+    'Qual é o personagem, conhecido por ser sarcástico, engraçado, violento e imprevisível, que voltou para o Universo Marvel após a Disney comprar a Fox na última década?',
+    'Deadpool'
+)
+
+criaCartao(
+    'Marvel',
+    'No filme Vingadores: Ultimato, quais são as jóias do infinito que o Capitão América viaja no tempo para devolver aos seus lugares de origem?',
+    'Pedra do Tempo, Pedra da Realidade e Pedra do Poder'
+)
+
+criaCartao(
+    'Marvel',
+    'Qual foi o primeiro filme da Marvel?',
+    'Homem de Ferro'
+)
+
+criaCartao(
+    'Marvel',
+    'Qual o significado do nome Marvel?',
+    'Maravilha'
+)
+criaCartao(
+    'Marvel',
+    'Quais foram os primeiros vingadores?',
+    'Homem-Formiga, Homem de Ferro, Thor, Vespa e Hulk'
+)
+criaCartao(
+    'Marvel',
+    'No filme "Homem-Aranha" de 2002, onde estava Peter Parker quando foi picado pela aranha que o transformou?',
+    'Em uma visita ao Laboratório de Genética na Columbia University, NY'
+)
